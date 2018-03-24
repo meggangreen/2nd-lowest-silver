a25c464a78aafcf3cf2cb10b66eeac056ac609dc
Meggan M Green <github@meggangreen.com>
2018-03-24 23:12:47 +0000

    everything is working

--- slcsp.py
+++ slcsp.py
@@ -37,14 +37,14 @@ def get_rate_areas(zips_csv):
 
     for line in lines:
         zip_code, state, _, _, rate_area = line.strip().split(',')
-        rate_areas[zip_code] = rate_areas.get(zip_code, [])
-        rate_areas[zip_code].append((state, rate_area))
+        rate_areas[zip_code] = rate_areas.get(zip_code, set([]))
+        rate_areas[zip_code].add((state, rate_area))
 
     return rate_areas
 
 
 def get_silver_rates(plans_csv):
-    """ Returns dictionary of state, rate areas and their sorted list of silver
+    """ Returns dictionary of state rate areas and their sorted list of silver
         plan rates.
 
     """
@@ -59,15 +59,15 @@ def get_silver_rates(plans_csv):
         if metal.lower() != 'silver':
             continue
         silver_rates[(state, rate_area)] = silver_rates.get((state, rate_area), [])
-        silver_rates[(state, rate_area)].append(int(rate))
+        silver_rates[(state, rate_area)].append(float(rate))
 
-    silver_rates = {area: sorted(rates) for area, rates in plans.items()}
+    silver_rates = {area: sorted(rates) for area, rates in silver_rates.items()}
 
     return silver_rates
 
 
 def get_slcsp_rates(silver_rates):
-    """ Returns dictionary of state, rate areas and the second-lowest rate. """
+    """ Returns dictionary of state rate areas and 2nd-lowest silver rate. """
 
     slcsp_rates = {}
 
@@ -103,16 +103,18 @@ def get_slcsp_for_zip(slcsp_csv, rate_areas, slcsp_rates):
         zip_code = zips_slcsp[i][:-1]
         if len(rate_areas.get(zip_code, [])) != 1:
             continue
-        rate_area = rate_areas[zip_code][0]
+        # Make a list of one without modifying the set
+        rate_area = list(rate_areas[zip_code])[0]
 
-        zips_slcsp[i] += slcsp_rates.get(str(rate_area), "")
+        zips_slcsp[i] += str(slcsp_rates.get(rate_area, ""))
 
     return zips_slcsp
 
 
 def replace_empty_slcsp_file_with_full(slcsp_csv, zips_csv, plans_csv):
-    """    Example code for making a temporary file and replacing another file with
-        it from StackOverflow: https://stackoverflow.com/a/39110
+    """ Given a file that contains only ZIP codes (slcsp_csv), overwrites it
+        with another file containing the original file's ZIP codes and any found
+        corresponding SLCSP rate values.
 
     """
 
@@ -121,27 +123,35 @@ def replace_empty_slcsp_file_with_full(slcsp_csv, zips_csv, plans_csv):
 
     # Get rate areas for each ZIP code in zips_csv
     rate_areas = get_rate_areas(zips_csv)
+    # Get SLCSP rates for each rate area from plans_csv
     slcsp_rates = get_slcsp_rates(get_silver_rates(plans_csv))
+    # Get corresponding SLCSP for each ZIP code in slcsp_csv
     zips_slcsp = get_slcsp_for_zip(slcsp_csv, rate_areas, slcsp_rates)
 
-    # Make temporary file and write to it
+    # Make temporary file and write to it;
+    # example from https://stackoverflow.com/a/39110
     temp, abs_path = mkstemp()
     with fdopen(temp, 'w') as new_file:
         for each_zip_slcsp in zips_slcsp:
-            new_file.write(each_zip_slcsp)
+            new_file.write(each_zip_slcsp + "\n")
     # Remove original file
-    remove('tester.csv')  # remove(slcsp_csv)
+    remove(slcsp_csv)
     # Move temp file to orignal file location
-    move(abs_path, 'tester.csv')  # move(abs_path, slcsp_csv)
+    move(abs_path, slcsp_csv)
 
 
 def has_all_files(file_names):
     """ Returns True/False if all necessary files are in program folder. """
 
+    if not file_names:
+        return False
+
     missing = 0
+    dir_files = set([f.name for f in pathlib.Path('.').glob('*.csv')])
+    # import pdb; pdb.set_trace()
 
     for f_name in file_names:
-        if f_name not in pathlib.Path('.').glob():
+        if f_name not in dir_files:
             missing += 1
 
     return missing == 0
@@ -155,9 +165,9 @@ if __name__ == '__main__':
     if has_all_files(file_names):
         slcsp_csv, zips_csv, plans_csv = file_names
         replace_empty_slcsp_file_with_full(slcsp_csv, zips_csv, plans_csv)
-        print("\n\nProcess complete. Please check 'tester.csv'.".format(slcsp_csv))
+        print("\n\nProcess complete. Please check '{}'.".format(slcsp_csv))
     else:
         print("\n\nEnsure the necessary files are in the program folder.")
         for f_name in file_names:
             print(f_name)
-
+        print("\n")
