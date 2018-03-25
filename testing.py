2d5a78f5c34622231aa34e2446001f220f0152f4
Meggan M Green <github@meggangreen.com>
2018-03-25 01:48:21 +0000

    writing unit tests

--- testing.py
+++ testing.py
@@ -1,1 +1,29 @@
-testing.py
+""" Testing Module """
+
+import unittest as UT
+from os import remove
+from slcsp import *
+import dataset
+
+def setUpModule():
+    """ Create dummy files (similar to a test database). """
+
+    dataset.create_test_data()
+
+
+def tearDownModule():
+    """ Delete dummy files. """
+
+    for file_name in dataset.test_files:
+        remove(file_name)
+
+
+class MainFunctionTests(UT.TestCase):
+
+    def test_
+
+
+################################################################################
+
+if __name__ == '__main__':
+    UT.main()
